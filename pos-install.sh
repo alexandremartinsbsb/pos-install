4a28a4518ef1cc71873b38264803ef93a11f5de7
Alexandre Martins da Silva <alexandre.martinsbsb@gmail.com>
2020-06-18 02:30:45 -0300

    update

--- pos-install.sh
+++ pos-install.sh
@@ -19,7 +19,8 @@ echo "deb-src http://ftp.br.debian.org/debian/ buster-updates main contrib non-f
 echo "deb http://http.debian.net/debian/ buster-backports main contrib non-free" | sudo tee -a /etc/apt/sources.list
 
 sudo apt update
-sudo apt upgrade -y
+sudo apt list --upgradable -a
+sudo apt full-upgrade -y
 
 ## Dependencias
 sudo apt-get install sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev -y
@@ -29,7 +30,8 @@ sudo apt install git openssl ca-certificates -y
 sudo apt install curl
 
 sudo apt update
-sudo apt upgrade -y
+sudo apt list --upgradable -a
+sudo apt full-upgrade -y
 
 ##Node & Npn
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
@@ -47,7 +49,9 @@ sudo rm /var/cache/apt/archives/lock
 sudo dpkg --add-architecture i386
 
 ## Atualizando o repositório ##
-sudo apt update -y
+sudo apt update
+sudo apt list --upgradable -a
+sudo apt full-upgrade -y
 
 ## Flameshot
 sudo apt install flameshot -y
@@ -61,6 +65,11 @@ hello-world
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+## Atualizando o repositório ##
+sudo apt update
+sudo apt list --upgradable -a
+sudo apt full-upgrade -y
 
 ## Instalar o Git
 sudo apt install git -y
@@ -83,7 +92,9 @@ sudo apt install teams -y
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
 ## Atualizando o repositório depois da adição de novos repositórios ##
-sudo apt update -y
+sudo apt update
+sudo apt list --upgradable -a
+sudo apt full-upgrade -y
 
 ## Download e instalaçao de programas externos ##
 mkdir "$DIRETORIO_DOWNLOADS"
@@ -102,19 +113,20 @@ flatpak install flathub org.gnome.meld -y
 flatpak install flathub com.syntevo.SmartGit -y
 flatpak install flathub org.videolan.VLC -y
 flatpak install flathub org.gabmus.hydrapaper
+flatpak install flathub org.telegram.desktop
+flatpak install flathub com.gigitux.gtkwhats
+flatpak install flathub com.skype.Client
+flatpak install flathub com.slack.Slack
+flatpak install flathub com.visualstudio.code
+flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
+flatpak install flathub com.sublimetext.three
+flatpak install flathub com.getpostman.Postman
+flatpak install flathub com.spotify.Client
 
 ## Instalando pacotes Snap ##
-sudo snap install telegram-desktop
-sudo snap install whatsdesk
-sudo snap install skype --classic
-sudo snap install slack --classic
-sudo snap install code --classic
 sudo snap install robo3t-snap
-sudo snap install intellij-idea-community --classic
-sudo snap install sublime-text --classic
-sudo snap install postman
-sudo snap install spotify
 sudo snap install photogimp
+
 # ---------------------------------------------------------------------- #
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
@@ -123,7 +135,9 @@ sudo rm -rf "$DIRETORIO_DOWNLOADS"
 sudo rm -rf /var/cache/snapd
 sudo rm -rf ~/snap
 sudo apt-get -f install -y
-sudo apt update && sudo apt dist-upgrade -y
+sudo apt update
+sudo apt list --upgradable -a
+sudo apt full-upgrade -y
 flatpak update
 sudo apt autoclean
 sudo apt autoremove -y
