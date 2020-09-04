395d915c7e09d678170b259d8f9c55fb12849c28
alexandre.martinsbsb <alexandre.martinsbsb@gmail.com>
2020-09-04 17:17:43 -0300

    update

--- pop_os.sh
+++ pop_os.sh
@@ -3,8 +3,10 @@ echo "Iniciando script"
 # ----------------------------- VARIÁVEIS ----------------------------- #
 
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_GITHUB_DESKTOP="https://github.com/shiftkey/desktop/releases/download/release-2.5.3-linux1/GitHubDesktop-linux-2.5.3-linux1.deb"
+URL_GITHUB_DESKTOP="https://github.com/shiftkey/desktop/releases/download/release-2.5.4-linux1/GitHubDesktop-linux-2.5.4-linux1.deb"
 URL_TEAMVIEW="https://download.teamviewer.com/download/linux/teamviewer_amd64.deb"
+URL_ETCHER="https://github.com/balena-io/etcher/releases/download/v1.5.106/balena-etcher-electron_1.5.106_amd64.deb"
+URL_VSCODE="https://go.microsoft.com/fwlink/?LinkID=760868"
 
 DIRETORIO_DOWNLOADS="/home/$USER/Downloads/programas"
 
@@ -25,11 +27,13 @@ sudo apt autoremove -y
 
 ## Dependencias
 sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
-sudo apt-get install sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev -y
+sudo apt-get install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev -y
 sudo apt install g++ build-essential qt5-default qt5-qmake qttools5-dev-tools -y
 sudo apt install libqt5dbus5 libqt5network5 libqt5core5a libqt5widgets5 libqt5gui5 libqt5svg5-dev -y
+sudo apt install gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils -y
 sudo apt install git openssl ca-certificates -y
-sudo apt install curl
+sudo apt install curl -y
+sudo apt install -y sassc
 
 ## Atualização
 sudo apt update
@@ -42,7 +46,7 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 ## Driver
-sudo apt install nvidia-legacy-390xx-driver -y
+##sudo apt install nvidia-legacy-390xx-driver -y
 
 # ----------------------------- REQUISITOS ----------------------------- #
 
@@ -51,9 +55,6 @@ sudo apt update
 sudo apt list --upgradable -a
 sudo apt full-upgrade -y
 sudo apt autoremove -y
-
-## Etcher
-sudo apt-get install balena-etcher-electron -y
 
 ## Gdebi
 sudo apt install gdebi -y
@@ -64,7 +65,6 @@ sudo apt install flameshot -y
 ## Snap
 sudo apt install snapd -y
 snap install hello-world
-hello-world
 
 ## Flatpak
 #sudo apt install flatpak -y
@@ -85,7 +85,9 @@ ssh-keygen -o
 cat ~/.ssh/id_rsa.pub
 
 ## Instala o Java
-sudo apt install default-jdk -y
+sudo apt-get install openjdk-11-jdk -y
+export JAVA_HOME=/usr/lib/jvm/openjdk-11-jdk
+export PATH=$PATH:$JAVA_HOME/bin
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
 ## Atualização
@@ -99,37 +101,38 @@ mkdir "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GOOGLE_CHROME"   -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_GITHUB_DESKTOP"  -P "$DIRETORIO_DOWNLOADS"
 wget -c "$URL_TEAMVIEW"        -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_ETCHER"          -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_VSCODE"          -P "$DIRETORIO_DOWNLOADS"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg --force-depends -i $DIRETORIO_DOWNLOADS/*.deb
 
 ## Instalando pacotes Flatpak ##
-flatpak install flathub com.obsproject.Studio -y
-flatpak install flathub io.dbeaver.DBeaverCommunity -y
-flatpak install flathub com.github.muriloventuroso.easyssh -y
-flatpak install flathub org.gnome.meld -y
-flatpak install flathub com.syntevo.SmartGit -y
-flatpak install flathub com.syntevo.SmartSynchronize -y
-flatpak install flathub org.videolan.VLC -y
-flatpak install flathub org.gabmus.hydrapaper -y
-flatpak install flathub org.telegram.desktop -y
-flatpak install flathub com.gigitux.gtkwhats -y
-flatpak install flathub com.skype.Client -y
-flatpak install flathub com.slack.Slack -y
-flatpak install flathub com.visualstudio.code -y
-flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community -y
-flatpak install flathub com.sublimetext.three -y
-flatpak install flathub com.getpostman.Postman -y
-flatpak install flathub com.spotify.Client -y
-flatpak install flathub org.eclipse.Java -y
-flatpak install flathub com.axosoft.GitKraken -y
-flatpak install flathub com.microsoft.Teams -y
-flatpak install flathub com.google.AndroidStudio -y
+sudo flatpak install flathub io.dbeaver.DBeaverCommunity -y
+sudo flatpak install flathub com.github.muriloventuroso.easyssh -y
+sudo flatpak install flathub org.gnome.meld -y
+sudo flatpak install flathub com.syntevo.SmartGit -y
+sudo flatpak install flathub com.syntevo.SmartSynchronize -y
+sudo flatpak install flathub org.gabmus.hydrapaper -y
+sudo flatpak install flathub com.microsoft.Teams -y
 
 ## Instalando pacotes Snap ##
 sudo snap install robo3t-snap
 sudo snap install photogimp
-sudo snap install flutter --classic
+sudo snap install obs-studio
+sudo snap install vlc
+sudo snap install telegram-desktop
+sudo snap install whatsdesk
+sudo snap install skype --classic
+sudo snap install slack --classic
+sudo snap install code --classic
+sudo snap install intellij-idea-community --classic
+sudo snap install sublime-text --classic
+sudo snap install postman
+sudo snap install spotify
+sudo snap install eclipse --classic
+sudo snap install gitkraken --classic
+sudo snap install android-studio --classic
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
 ## Finalização, atualização e limpeza##
