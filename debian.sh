dfb937a8c282cde530aff8c60668d6520420288a
Alexandre Martins da Silva <alexandre.martinsbsb@gmail.com>
2022-02-10 21:50:34 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -145,6 +145,6 @@ sudo apt update
 sudo flatpak update -y
 
 ## Atualização
-sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /var/lib/apt/lists/partial/* && sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y sudo flatpak repair && sudo flatpak update -y && sudo flatpak uninstall --unused -y
+sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /var/lib/apt/lists/partial/* && sudo apt update -y && sudo apt list --upgradable && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y sudo flatpak repair && sudo flatpak update -y && sudo flatpak uninstall --unused -y
 # ---------------------------------------------------------------------- #
 echo "Script Finalizado"
