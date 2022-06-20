df1306431b67a11a5aeb181fc6257bee603abc5b
Alexandre Martins da Silva <alexandre.martinsbsb@gmail.com>
2022-06-20 19:03:58 -0300

    UPDATE

--- debian.sh
+++ debian.sh
@@ -3,13 +3,14 @@ echo "Iniciando script"
 # ----------------------------- VARIÁVEIS ----------------------------- #
 
 URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_GITHUB_DESKTOP="https://github.com/shiftkey/desktop/releases/download/release-2.9.6-linux1/GitHubDesktop-linux-2.9.6-linux1.deb"
+URL_GITHUB_DESKTOP="https://github.com/shiftkey/desktop/releases/download/release-3.0.1-linux1/GitHubDesktop-linux-3.0.1-linux1.deb"
 URL_TEAMVIEW="https://download.teamviewer.com/download/linux/teamviewer_amd64.deb"
-URL_ETCHER="https://github.com/balena-io/etcher/releases/download/v1.7.3/balena-etcher-electron_1.7.3_amd64.deb"
-URL_COMPASS="https://downloads.mongodb.com/compass/mongodb-compass_1.30.1_amd64.deb"
+URL_ETCHER="https://github.com/balena-io/etcher/releases/download/v1.7.9/balena-etcher-electron_1.7.9_amd64.deb"
+URL_COMPASS="https://downloads.mongodb.com/compass/mongodb-compass_1.32.2_amd64.deb"
 URL_SKYPE="https://go.skype.com/skypeforlinux-64.deb"
 URL_DBEAVER="https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb"
-URL_SMARTGIT="https://www.syntevo.com/downloads/smartgit/smartgit-21_2_2.deb"
+URL_SMARTGIT="https://www.syntevo.com/downloads/smartgit/smartgit-21_2_3.deb"
+URL_WHATSAPP="https://github.com/meetfranz/franz/releases/download/v5.9.2/franz_5.9.2_amd64.deb"
 
 DIRETORIO_DOWNLOADS="./programas"
 
@@ -35,6 +36,7 @@ sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /var/lib/apt/lists/partial/* && 
 
 ## Dependencias
 sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
+sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 5E3C45D7B312C643
 sudo apt install '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev -y
 sudo apt install g++ build-essential qt5-qmake qttools5-dev-tools -y
 sudo apt install libqt5dbus5 libqt5network5 libqt5core5a libqt5widgets5 libqt5gui5 libqt5svg5-dev -y
@@ -52,7 +54,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['']"
 sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /var/lib/apt/lists/partial/* && sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y
 
 ##Node & Npn
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
 
 ## Driver
@@ -77,7 +79,7 @@ sudo flatpak remote-add --if-not-exists kdeapps --from https://distribute.kde.or
 sudo flatpak update -y
 
 ## Instalar Spotify
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update -y && sudo apt install spotify-client -y
 
@@ -95,8 +97,8 @@ ssh-keygen -o
 cat ~/.ssh/id_rsa.pub
 
 ## Instala o Java
-sudo apt install openjdk-17-jdk -y
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+sudo apt install openjdk-18-jdk -y
+export JAVA_HOME=/usr/lib/jvm/java-18-openjdk-amd64
 export PATH=$PATH:$JAVA_HOME/bin
 
 ## Instalando neofetch
@@ -117,13 +119,14 @@ wget -c "${URL_COMPASS}"         -P "${DIRETORIO_DOWNLOADS}"
 wget -c "${URL_SKYPE}"           -P "${DIRETORIO_DOWNLOADS}"
 wget -c "${URL_DBEAVER}"         -P "${DIRETORIO_DOWNLOADS}"
 wget -c "${URL_SMARTGIT}"        -P "${DIRETORIO_DOWNLOADS}"
+wget -c "${URL_WHATSAPP}"        -P "${DIRETORIO_DOWNLOADS}"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg --force-depends -i ${DIRETORIO_DOWNLOADS}/*.deb
 
 ## Instalando pacotes Flatpak ##
 #sudo flatpak install flathub io.dbeaver.DBeaverCommunity -y
-#sudo flatpak install flathub com.github.muriloventuroso.easyssh -y
+sudo flatpak install flathub com.github.muriloventuroso.easyssh -y
 sudo flatpak install flathub org.gnome.meld -y
 #sudo flatpak install flathub com.syntevo.SmartGit -y
 #sudo flatpak install flathub org.gabmus.hydrapaper -y
@@ -135,7 +138,6 @@ sudo flatpak install flathub com.getpostman.Postman -y
 #sudo flatpak install flathub com.spotify.Client -y
 #sudo flatpak install flathub org.eclipse.Java -y
 #sudo flatpak install flathub com.google.AndroidStudio -y
-sudo flatpak install flathub io.bit3.WhatsAppQT -y
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
 ## Finalização, atualização e limpeza##
