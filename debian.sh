8b31924e946d7ede9c6b9aa3a753590078b64af3
Alexandre Martins da Silva <alexandre.martinsbsb@gmail.com>
2022-07-15 11:49:55 -0300

    atualizacao

--- debian.sh
+++ debian.sh
@@ -12,13 +12,44 @@ URL_DBEAVER="https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb"
 URL_SMARTGIT="https://www.syntevo.com/downloads/smartgit/smartgit-21_2_3.deb"
 URL_WHATSAPP="https://github.com/meetfranz/franz/releases/download/v5.9.2/franz_5.9.2_amd64.deb"
 
-DIRETORIO_DOWNLOADS="./programas"
+DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
 
-# ---------------------------------------------------------------------- #
+VERMELHO='\e[1;91m'
+VERDE='\e[1;92m'
+SEM_COR='\e[0m'
+
+PROGRAMAS_PARA_INSTALAR=(
+    nvidia-legacy-390xx-driver
+    gdebi
+    flameshot
+    vlc
+    git
+    git-flow
+    neofetch
+)
+
+# ----------------------------- FUNCOES -------------------------------- #
+
+atualiza(){
+    sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /var/lib/apt/lists/partial/* && sudo apt update -y && sudo apt list --upgradable && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y sudo flatpak repair && sudo flatpak update -y && sudo flatpak uninstall --unused -y
+}
+
+# ----------------------------- FUNCOES -------------------------------- #
+
+testa_internet(){
+    if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+        echo -e "${VERMELHO}[ERROR] - O computador nao tem conexao com a internet. Verificar a situacao da rede.${SEM_COR}"
+        exit 1
+    else
+        echo -e "${VERDE}[INFO] - Conexao com a internet funcionando normalmente.${SEM_COR}"
+    fi
+}
 
 ## Removendo travas eventuais do apt ##
-sudo rm /var/lib/dpkg/lock-frontend
-sudo rm /var/cache/apt/archives/lock
+remove_travas_apt(){
+    sudo rm /var/lib/dpkg/lock-frontend
+    sudo rm /var/cache/apt/archives/lock
+}
 
 ## Adicionando/Confirmando arquitetura de 32 bits ##
 ## sudo dpkg --add-architecture i386
@@ -58,7 +89,7 @@ curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
 
 ## Driver
-sudo apt install nvidia-legacy-390xx-driver -y
+##sudo apt install nvidia-legacy-390xx-driver -y
 
 # ----------------------------- REQUISITOS ----------------------------- #
 
@@ -66,10 +97,10 @@ sudo apt install nvidia-legacy-390xx-driver -y
 sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /var/lib/apt/lists/partial/* && sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y
 
 ## Gdebi
-sudo apt install gdebi -y
+##sudo apt install gdebi -y
 
 ## Flameshot
-sudo apt install flameshot -y
+#sudo apt install flameshot -y
 
 ## Flatpak
 sudo apt install flatpak -y
@@ -79,18 +110,18 @@ sudo flatpak remote-add --if-not-exists kdeapps --from https://distribute.kde.or
 sudo flatpak update -y
 
 ## Instalar Spotify
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt update -y && sudo apt install spotify-client -y
+##curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
+##echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+##sudo apt update -y && sudo apt install spotify-client -y
 
 ## Instalar VLC
-sudo apt install vlc -y
+##sudo apt install vlc -y
 
 ## Instalar o Git
-sudo apt install git -y
+#sudo apt install git -y
 
 # Instala Git Flow
-sudo apt install git-flow -y
+##sudo apt install git-flow -y
 
 ## Instalar o SSH(Git)
 ssh-keygen -o
@@ -102,51 +133,88 @@ export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export PATH=$PATH:$JAVA_HOME/bin
 
 ## Instalando neofetch
-sudo apt install neofetch -y
-neofetch
+##sudo apt install neofetch -y
+##neofetch
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
 ## Atualização
 sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /var/lib/apt/lists/partial/* && sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y
 
 ## Download e instalaçao de programas externos ##
-mkdir "${DIRETORIO_DOWNLOADS}"
-wget -c "${URL_GOOGLE_CHROME}"   -P "${DIRETORIO_DOWNLOADS}"
-wget -c "${URL_GITHUB_DESKTOP}"  -P "${DIRETORIO_DOWNLOADS}"
-wget -c "${URL_TEAMVIEW}"        -P "${DIRETORIO_DOWNLOADS}"
-wget -c "${URL_ETCHER}"          -P "${DIRETORIO_DOWNLOADS}"
-wget -c "${URL_COMPASS}"         -P "${DIRETORIO_DOWNLOADS}"
-wget -c "${URL_SKYPE}"           -P "${DIRETORIO_DOWNLOADS}"
-wget -c "${URL_DBEAVER}"         -P "${DIRETORIO_DOWNLOADS}"
-wget -c "${URL_SMARTGIT}"        -P "${DIRETORIO_DOWNLOADS}"
-wget -c "${URL_WHATSAPP}"        -P "${DIRETORIO_DOWNLOADS}"
+baixa_instala_debs(){
+    echo -e "${VERDE}[INFO] - Baixando instalando pacotes .deb${SEM_COR}"
+    mkdir "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_GOOGLE_CHROME"   -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_GITHUB_DESKTOP"  -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_TEAMVIEW"        -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_ETCHER"          -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_COMPASS"         -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_SKYPE"           -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_DBEAVER"         -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_SMARTGIT"        -P "$DIRETORIO_DOWNLOADS"
+    wget -c "$URL_WHATSAPP"        -P "$DIRETORIO_DOWNLOADS"
 
-## Instalando pacotes .deb baixados na sessão anterior ##
-sudo dpkg --force-depends -i ${DIRETORIO_DOWNLOADS}/*.deb
+    echo -e "${VERDE}[INFO] - Instalando pacotes .deb baixados${SEM_COR}"
+
+    ## Instalando pacotes .deb baixados na sessão anterior ##
+    sudo dpkg --force-depends -i $DIRETORIO_DOWNLOADS/*.deb
+
+    ## Instalando programas via apt
+    echo -e "${VERDE}[INFO] - Instalando pacotes via apt do repositorio${SEM_COR}"
+
+    for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
+        if ! dpkg -l | grep -q $nome_do_programa; then # verifica se ja esta instalado
+            sudo apt install "$nome_do_programa" -y
+        else
+            echo "[JA_ESTA_INSTALADO] - $nome_do_programa"
+        fi
+    done
+}
 
 ## Instalando pacotes Flatpak ##
-#sudo flatpak install flathub io.dbeaver.DBeaverCommunity -y
-sudo flatpak install flathub com.github.muriloventuroso.easyssh -y
-sudo flatpak install flathub org.gnome.meld -y
-#sudo flatpak install flathub com.syntevo.SmartGit -y
-#sudo flatpak install flathub org.gabmus.hydrapaper -y
-sudo flatpak install flathub org.telegram.desktop -y
-sudo flatpak install flathub com.slack.Slack -y
-#sudo flatpak install flathub com.jetbrains.IntelliJ-IDEA-Ultimate -y
-sudo flatpak install flathub com.sublimetext.three -y
-sudo flatpak install flathub com.getpostman.Postman -y
-#sudo flatpak install flathub com.spotify.Client -y
-#sudo flatpak install flathub org.eclipse.Java -y
-#sudo flatpak install flathub com.google.AndroidStudio -y
+instala_flatpaks(){
+    echo -e "${VERDE}[INFO] - Instalando pacotes via Flatpak${SEM_COR}"
 
-# ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
+    #flatpak install flathub io.dbeaver.DBeaverCommunity -y
+    flatpak install flathub com.github.muriloventuroso.easyssh -y
+    flatpak install flathub org.gnome.meld -y
+    #flatpak install flathub com.syntevo.SmartGit -y
+    #flatpak install flathub org.gabmus.hydrapaper -y
+    flatpak install flathub org.telegram.desktop -y
+    flatpak install flathub com.slack.Slack -y
+    #flatpak install flathub com.jetbrains.IntelliJ-IDEA-Ultimate -y
+    flatpak install flathub com.sublimetext.three -y
+    flatpak install flathub com.getpostman.Postman -y
+    flatpak install flathub com.spotify.Client -y
+    #flatpak install flathub org.eclipse.Java -y
+    #flatpak install flathub com.google.AndroidStudio -y
+    flatpak install flathub org.gimp.GIMP -y
+}
+
+## Instalando pacotes Snap ##
+instala_snaps(){
+    echo -e "${VERDE}[INFO] - Instalando pacotes via Snap${SEM_COR}"
+
+    #sudo snap install NOME
+}
+
+# ----------------------------- POS-INSTALACAO ----------------------------- #
 ## Finalização, atualização e limpeza##
-sudo rm -rf "$DIRETORIO_DOWNLOADS"
-sudo apt -f install -y
-sudo apt update
-sudo flatpak update -y
+limpa_sistema(){
+    sudo rm -rf "$DIRETORIO_DOWNLOADS"
+    sudo apt -f install -y
+}
 
-## Atualização
-sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /var/lib/apt/lists/partial/* && sudo apt update -y && sudo apt list --upgradable && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y sudo flatpak repair && sudo flatpak update -y && sudo flatpak uninstall --unused -y
-# ---------------------------------------------------------------------- #
-echo "Script Finalizado"
+# ----------------------------- EXECUCAO ----------------------------- #
+
+remove_travas_apt
+testa_internet
+atualiza
+baixa_instala_debs
+instala_flatpaks
+instala_snaps
+limpa_sistema
+atualiza
+
+#finalizacao
+echo -e "${VERDE}[INFO] - Script finalizado, instalacao concluida! :)${SEM_COR}"
