3e1ffe2f5b8364ad7ec0a94068c85c2426082dfe
Alexandre Martins da Silva <alexandre.martinsbsb@gmail.com>
2023-02-22 21:29:37 -0300

    autalização

--- debian/98-Gnome.sh
+++ debian/98-Gnome.sh
@@ -10,7 +10,7 @@ SEM_COR='\e[0m'
 echo -e "${VERDE}[INFO] - Atualizando o sistema${SEM_COR}"
 sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /var/lib/apt/lists/partial/* && sudo rm -f /var/lib/dpkg/lock && sudo apt update -y && sudo apt list --upgradable && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y sudo flatpak repair && sudo flatpak update -y && sudo flatpak uninstall --unused -y
 
-sudo apt install gnome-shell gnome-session gnome-terminal nautilus network-manager-gnome gdm3 xinit gnome-control-center xdg-user-dirs-gtk zstd
+sudo apt install gnome-shell gnome-session gnome-terminal nautilus network-manager-gnome gdm3 xinit gnome-control-center xdg-user-dirs-gtk zstd -y
 
 #finalizacao
 echo -e "${VERDE}[INFO] - Script finalizado, instalacao concluida! :)${SEM_COR}"
